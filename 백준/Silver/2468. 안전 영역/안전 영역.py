bf364117ab94db599a8fae653e20b376c0a5496d
dbscks97 <75676309+dbscks97@users.noreply.github.com>
2023-04-13 01:49:54 +0900

    [Silver I] Title: 안전 영역, Time: 596 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,56 +1,47 @@
 import sys
 from collections import deque
 
-def bfs(x,y):
-    global result
+def bfs(i,j,h):
+    
     queue = deque()
-    queue.append((x,y))
-    visited[x][y]=1
+    queue.append((i,j))
+    v[i][j] = 1
     
     while queue:
-        p,q=queue.popleft()
-        dx= [0,1,0,-1]
-        dy = [1,0,-1,0]
+        ci,cj = queue.popleft()
         
-        for i in range(4):
-            nx = p + dx[i]
-            ny = q + dy[i]
+        for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
+            ni,nj = ci+di,cj+dj
             
-            if nx<= -1 or nx>= n or ny<= -1 or ny>=n:
-                continue
-            if graph[nx][ny] > k and not visited[nx][ny]:
-            
-                visited[nx][ny]=1
-                queue.append((nx,ny))
-          
-    result +=1        
+            if 0<=ni<n and 0<=nj<n and v[ni][nj]==0 and graph[ni][nj]>h:
+                queue.append((ni,nj))
+                v[ni][nj]=1
         
-    return True
 
+
+
+def solve(h):
+    cnt = 0
     
-
-n= int(sys.stdin.readline())
-
-
-
-graph=[]
-
-for i in range(n):
-    graph.append(list(map(int,input().split())))
-
-
-   
-
-sum=1
-for k in range(1,101):
-    result=0
-    visited=[[0]*n for _ in range(n)]
     for i in range(n):
         for j in range(n):
-            if graph[i][j] >k and not visited[i][j]: 
-                bfs(i,j)
+            if v[i][j]==0 and graph[i][j]>h:
+                bfs(i,j,h)
+                cnt+=1
+    return cnt
+
+if __name__=='__main__':
+    n = int(sys.stdin.readline())
+    ans = 1
+
+    graph = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+
+
+
+    for h in range(1,101):
+        v=[[0]*n for _ in range(n)]
+        ans = max(ans,solve(h))
     
-    sum = max(sum,result)                
-
-
-print(sum)
+print(ans)
+    
+            
