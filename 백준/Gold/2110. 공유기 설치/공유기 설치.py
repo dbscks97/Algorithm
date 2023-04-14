6bcb35917ada56ad84f09d484f18ec2519467421
dbscks97 <75676309+dbscks97@users.noreply.github.com>
2023-04-14 17:46:53 +0900

    [Gold IV] Title: 공유기 설치, Time: 580 ms, Memory: 38984 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -0,0 +1,31 @@
+from sys import stdin as s
+import sys
+
+#s = open('input.txt','rt')
+
+N, C = list(map(int, s.readline().split()))  #집 개수 N, 공유기 C개
+
+arr = [int(s.readline().rstrip()) for _ in range(N)]  # 공유기 거리
+arr.sort()  # 이분탐색을 위한 정렬
+# [1, 2, 4, 8, 9]
+# 두 공유기 사이의 최대 거리를 이분탐색으로
+start = 1  # 최소 거리
+end = arr[-1] - arr[0]  # 가장 큰 값 - 가장 작은 값 차이; 최대 거리
+result = 0
+
+while(start <= end):
+    mid = (start + end) // 2  # 가장 클수 있는 거리의 중간값 4
+    cur = arr[0] # 첫번째 값부터 시작
+    cnt = 1
+    for i in range(1,len(arr)): # 두번째 공유기부터 마지막까지
+        if arr[i] >= cur + mid:  # 두번째 값이 시작값+거리 보다 크면
+            cur = arr[i]  # 공유기를 설치
+            cnt += 1
+    if cnt >= C:  # 갯수가 공유기 개수보다 많으면
+        start = mid + 1 #차이를 키워주기
+        result = mid
+    else:  # 더 적게 설치되면
+        end = mid - 1  # 차이를 줄여주기
+        
+print(result)
+    
