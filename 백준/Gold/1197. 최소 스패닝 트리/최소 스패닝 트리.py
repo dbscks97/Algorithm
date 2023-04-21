ba44ef2d82775428e2156dd9f3804f5ef8afe602
Park Yun Chan <dbscks93@naver.com>
2023-04-21 21:56:49 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 268 ms, Memory: 49356 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -2,40 +2,43 @@ from sys import stdin as s
 
 #s = open('input.txt','rt')
 
+
 def find_parent(x):
     if parent[x] == x:
-        return x    
-    parent[x] = find_parent(parent[x])
+        return x
+    else:
+       parent[x] = find_parent(parent[x])
     return parent[x]
-    
 
 def union_parent(A,B):
-    A = find_parent(A)
-    B = find_parent(B)
+    A=find_parent(A)
+    B=find_parent(B)
     
     if A<B:
         parent[B]=A
     else:
         parent[A]=B
-
-
+                       
 if __name__=='__main__':
+    
     V,E = map(int,s.readline().split())
     
-    #부모 테이블상에서, 부모를 자기 자신으로 초기화
-    parent = [i for i in range(V+1)]
-    answer = 0
-    edges=[]
+    edge = []
+    answer =0 
+    
+    parent =[i for i in range(V+1)]
+    
     for i in range(E):
         A,B,C = map(int,s.readline().split())
-        edges.append((A,B,C))
-    edges.sort(key=lambda x: x[2])
+        edge.append((A,B,C))
+    edge.sort(key=lambda x:x[2])
     
-    for A,B,C in edges:    
+    for A,B,C in edge:
         if find_parent(A) != find_parent(B):
             union_parent(A,B)
-            answer += C 
+            answer += C
     
-    print(answer)   
-    
+    print(answer)    
+
+        
     
