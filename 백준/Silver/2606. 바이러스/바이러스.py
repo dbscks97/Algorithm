2bca21bf159f6a0414323f3c7174a2969ea4dd29
Park Yun Chan <dbscks93@naver.com>
2023-04-24 20:10:46 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,45 +1,27 @@
 from sys import stdin as s
 
-#s = open('input.txt','rt')
-
-
-def find_parent(x):
-    if parent[x] == x:
-        return x
-    else:
-        parent[x] = find_parent(parent[x])
-    return parent[x]
-
-def union(A,B):
-    A=find_parent(A)
-    B=find_parent(B)
+def dfs(c):
+    global cnt
+    v[c]=1  #초기값 방문표시
     
-    if A<B:
-        parent[B]=A
-    else:
-        parent[A]=B
-        
-
+    for n in arr[c]:
+        if not v[n]:
+            v[n]=1
+            cnt+=1
+            dfs(n)
+            
 if __name__=='__main__':
     
-    N = int(s.readline())
-    M = int(s.readline())
-    parent = [i for i in range(N+1)]
-    edge =[]
+    V = int(s.readline())
+    E = int(s.readline())
     
-    for i in range(M):
+    arr = [[]for _ in range(V+1)]
+    v = [0]* (V+1)
+    cnt =0
+    for i in range(E):
         A,B = map(int,s.readline().split())
-        edge.append((A,B))
-    # edge.sort()
+        arr[A].append(B)
+        arr[B].append(A)
     
-    edge.sort(key=lambda x:x[1])
-    for i in range(2):
-        for i,j in edge:
-            if find_parent(i) != find_parent(j):
-                union(i,j) 
-       
-    print(parent.count(1)-1)
-    
-            
-    
-    
+    dfs(1)
+    print(cnt)
