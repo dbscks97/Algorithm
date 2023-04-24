184be40b7792d97dc8d67b3998fea1f6825be129
Park Yun Chan <dbscks93@naver.com>
2023-04-24 20:57:15 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1968 ms, Memory: 101160 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,45 +1,43 @@
-from sys import stdin as s
 from collections import deque
+import sys
+from sys import stdin as s
+sys.setrecursionlimit(10**6)
 
 
-def bfs(c):
-    q=deque()
-    q.append(c)
-    visit[c]=1
-    distance[c]=0
-    answer =[]
-    while q:
-        s = q.popleft()
-        
-        for n in adj[s]:
-            if not visit[n]:
-                visit[n]=1
-                q.append(n)
-                distance[n] = distance[s]+1
-                if distance[n] == K:
-                    answer.append(n)
-    if len(answer)==0:
-        print(-1)
-    else:
-        answer.sort()
-        for i in answer:
-            print(i,end='\n')
+def bfs(s):
+    global answer 
+    q = deque()
+    q.append(s)
+    v[s]=1
+    distance[s]=0
     
-
+    while q:
+        c = q.popleft()
+        
+        for n in arr[c]:
+            if not v[n]:
+                q.append(n)
+                v[n] = 1
+                distance[n] = distance[c]+1
+                if distance[n]==K:
+                    answer.append(n)
+            
 if __name__=='__main__':
     
-    N,M,K,X = map(int,s.readline().split())
-    
-    adj=[[]for _ in range(N+1)]
-    visit = [0] *(N+1)
+    N,M,K,X= map(int,s.readline().split())
+    v = [0] * (N+1)
     distance = [0] * (N+1)
-    
+    arr = [[]for _ in range(N+1)]
+    answer = []
     for i in range(1,M+1):
-        A,B= map(int,s.readline().split())
-        adj[A].append(B)
+        A,B = map(int,s.readline().split())
+        arr[A].append(B)
     
     
     bfs(X)
-    
-    
-    
+    answer.sort()
+    if answer:
+        for i in answer:
+            print(i)
+    else:
+        print(-1)
