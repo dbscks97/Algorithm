2d99b113504560f3cc24629c543ee249e72f2c4d
Park Yun Chan <dbscks93@naver.com>
2023-04-24 20:38:20 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,33 +1,39 @@
-from sys import stdin as s
 from collections import deque
+import sys
+from sys import stdin as s
+sys.setrecursionlimit(10**6)
 
 def bfs(si,sj,ei,ej):
-    global cnt
-    
     q=deque()
     q.append((si,sj))
-    visit[si][sj]=1
+    v[si][sj]=1
     
     while q:
         ci,cj = q.popleft()
         
-        if (ci,cj)==(ei,ej):
-            return visit[ei][ej]
+        if (ci,cj) ==  (ei,ej):
+            return v[ei][ej]
         
-        for di,dj in ((0,1),(1,0),(0,-1),(-1,0)):
-            ni,nj = ci+di, cj+dj
-            if 0<=ni<N and 0<=nj<M and arr[ni][nj]==1 and visit[ni][nj]==0:
+        di = [0,1,0,-1]
+        dj = [1,0,-1,0]
+        
+        for i in range(4):
+            ni = ci+di[i]
+            nj = cj+dj[i]
+            
+            if 0<=ni<N and 0<=nj<M and arr[ni][nj]==1 and v[ni][nj]==0:
+                v[ni][nj]=v[ci][cj]+1
                 q.append((ni,nj))
-                visit[ni][nj]=visit[ci][cj]+1
-    
-
+                            
 
 if __name__=='__main__':
-    N,M = map(int,s.readline().split())
     
-    arr=[list(map(int,s.readline().strip())) for _ in range(N)]
-
-    visit =[[0]*M for _ in range(N)]
-    ans=bfs(0,0,N-1,M-1)
+    N,M = map(int,s.readline().split())
+    arr = [list(map(int,s.readline().strip()))for _ in range(N)]
+    v = [[0] * M for _ in range(N)]
+    
+    ans = bfs(0,0,N-1,M-1)
     
     print(ans)
+    
+    
