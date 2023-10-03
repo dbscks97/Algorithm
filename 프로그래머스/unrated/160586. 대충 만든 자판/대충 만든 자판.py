02574aafdd01473ca99c494e3f2f31b0ca89ed08
Park Yun Chan <dbscks9793@gmail.com>
2023-10-03 16:52:37 +0900

    [unrated] Title: 대충 만든 자판, Time: 0.77 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,23 @@
+def solution(key, tar):
+    answer = []
+    dic = {}
+
+    for i in key:                           
+        tmp = list(set(list(i)))           
+        for j in tmp:                     
+            if(j in dic):                   
+                dic[j] = min(dic[j], i.index(j) + 1)
+            else:                           
+                dic[j] = i.index(j) + 1
+
+    for i in tar:                          
+        tmp = 0                             
+        for j in i:                        
+            if(j in dic):
+                tmp += dic[j]
+            else:                           
+                tmp = -1                  
+                break                
+        answer.append(tmp)                  
+
+    return answer
