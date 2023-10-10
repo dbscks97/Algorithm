41786b72bb4329eafd6dc9639bf843a37ff055ad
Park Yun Chan <dbscks9793@gmail.com>
2023-10-11 00:11:47 +0900

    [Silver IV] Title: 카드2, Time: 204 ms, Memory: 50928 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,32 +1,15 @@
-import sys
 from sys import stdin as s
 from collections import deque
 
-#s = open('input.txt','rt')
+ans = deque()
+N = int(s.readline())
 
-# 스택 객체 생성
-q = deque()
+for i in range(1,N+1):
+    ans.append(i)
 
-n = int(s.readline())
+while(len(ans)>1):
+    ans.popleft()
+    a = ans.popleft()
+    ans.append(a)
 
-for i in range(1,n+1):
-    q.append(i)
-
-
-
-
-
-while q:
-      
-    if len(q)==1:
-        print(q[0])
-        break
-    
-    else:
-        q.popleft()
-        temp =q.popleft()
-        q.append(temp)
-    
-
-    
-    
+print(ans[0])
