d5357a7cbdc6eafb341fee615ef82c5a07138e6f
Park Yun Chan <dbscks9793@gmail.com>
2024-01-19 10:14:24 +0900

    [Gold V] Title: 호텔, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -1,21 +1,14 @@
 from sys import stdin as s
 
 C,N = map(int,s.readline().split())
-arr=[]
-count=0
-result=0
+
+dp=[1e9]*(C+1000)
+dp[0] = 0
 for i in range(N):
-    cost,people = map(int,s.readline().split())
-    arr.append((cost,people))
-
-max_people = max(arr, key=lambda x: x[1])[1]
-
-dp = [float('inf')] * (C+max_people + 1)
-dp[0]=0
-
-for cost,people in arr:
-    for i in range(people,C+max_people+1):
-        dp[i]=min(dp[i-people]+cost,dp[i])
-
+    cost, people = map(int,s.readline().split())
+    
+    for j in range(1,C+1+people):
+        dp[j] = min(dp[j-people]+cost,dp[j])
+        
 print(min(dp[C:]))
-
+    
