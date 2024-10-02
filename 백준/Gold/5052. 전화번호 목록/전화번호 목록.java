cb3304f9f53c7b3e51136e3e7cb18e37cd6d1b5b
Park Yun Chan <dbscks9793@gmail.com>
2024-10-02 19:28:28 +0900

    [Gold IV] Title: 전화번호 목록, Time: 532 ms, Memory: 101036 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,13 +1,14 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int N = Integer.parseInt(br.readLine());
 
 		StringBuilder result = new StringBuilder();
@@ -22,13 +23,15 @@ public class Main {
 				phoneMap.put(phoneNumber, true);
 			}
 
-			// 접두어 체크
 			for (String number : phoneMap.keySet()) {
 				for (int j = 1; j < number.length(); j++) {
 					if (phoneMap.containsKey(number.substring(0, j))) {
 						isPrefixFree = false;
 						break;
 					}
+				}
+				if(!isPrefixFree){
+					break;
 				}
 			}
 
